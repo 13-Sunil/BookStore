49a0fc213fe7320e3a25d2ee4a4abe17b84514eb
13-Sunil <87757010+13-Sunil@users.noreply.github.com>
2022-07-08 19:09:40 +0530

    Add files via upload

---
+++ BookStore.Models/Models/ListResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookStore.Models.ViewModels;
+
+namespace BookStore.Models.Models
+{
+    public class ListResponse<T> where T : class
+    {
+
+            public List<T> Records { get; set; }
+
+            public int TotalRecords { get; set; }
+    }
+}
+
---
+++ BookStore.Repository/BaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using BookStore.Models.ViewModels;
+
+namespace BookStore.Repository
+{
+    public class BaseRepository
+    {
+       
+       protected readonly BookStoreContext _context = new BookStoreContext();
+        
+    }
+}
+
